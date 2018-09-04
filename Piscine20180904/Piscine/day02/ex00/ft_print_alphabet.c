505c2bd9d5555ec3d5a1b5c7f959b3543796366c
Nathalie CHIN <nchin@e3r6p12.42.fr>
2018-09-04 11:24:05 +0200

    Test

---
+++ Piscine20180904/Piscine/day02/ex00/ft_print_alphabet.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchin <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/01 00:56:39 by nchin             #+#    #+#             */
+/*   Updated: 2018/09/01 15:05:19 by nchin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void	ft_print_alphabet(void)
+{
+	int letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter += 1;
+	}
+}
+
+int		main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
